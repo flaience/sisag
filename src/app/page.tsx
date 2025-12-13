4fbd1227a730ec21145bd3ee78684c7986bc8412
luismollinetti <69463960+luismollinetti@users.noreply.github.com>
2025-12-13 16:37:41 -0300

    teste deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function HomePage() {
         animate={{ opacity: 1, y: 0 }}
         className="text-4xl font-bold text-primary"
       >
-        Bem-vindo à Clínica odontológica d zona fdodido{" "}
+        Bem-vindo à Clínica odontológica d zona nova
       </motion.h1>
       <p className="text-gray-600 text-lg">
         Por favor, selecione uma opção para continuar:
