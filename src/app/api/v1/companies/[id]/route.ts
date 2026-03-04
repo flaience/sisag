3944d894a47047551380d5a592d59ec605d7fd02
luismollinetti <69463960+luismollinetti@users.noreply.github.com>
2026-03-04 10:11:06 -0300

    update github secret

--- src/app/api/v1/companies/[id]/route.ts
+++ src/app/api/v1/companies/[id]/route.ts
@@ -1,3 +1,5 @@
+//src/app/api/v1/companies/[id]/route.ts
+
 export const runtime = "nodejs";
 
 import { NextResponse } from "next/server";
@@ -5,7 +7,7 @@ import { CompanyService } from "@/modules/companies/Company.service";
 
 export async function GET(
   _req: Request,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: Promise<{ id: string }> },
 ) {
   const { id } = await params;
 
@@ -20,7 +22,7 @@ export async function GET(
 
 export async function PUT(
   req: Request,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: Promise<{ id: string }> },
 ) {
   const { id } = await params;
   const body = await req.json();
@@ -36,7 +38,7 @@ export async function PUT(
 
 export async function DELETE(
   _req: Request,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: Promise<{ id: string }> },
 ) {
   const { id } = await params;
 
