5d435f8fc6fa9994e1f87b2438f8d50a46111724
luismollinetti <69463960+luismollinetti@users.noreply.github.com>
2026-07-01 17:26:45 -0300

    fix(ui): export SISAG timeline component

--- src/components/sisag/index.ts
+++ src/components/sisag/index.ts
@@ -1,5 +1,9 @@
 export { SisagPage } from "./layout/SisagPage";
 export { SisagSection } from "./layout/SisagSection";
+
 export { SisagQuickAccessCard } from "./cards/SisagQuickAccessCard";
 export { SisagPriorityCard } from "./cards/SisagPriorityCard";
 export { SisagMetricCard } from "./cards/SisagMetricCard";
+
+export { SisagTimeline } from "./timeline/SisagTimeline";
+export type { SisagTimelineItem } from "./timeline/SisagTimeline";
